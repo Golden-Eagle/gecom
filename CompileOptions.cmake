c2bd5bbbe9805696dc0e50b8e78076cdbcfeac0e
Ben Allen <benos.9k@gmail.com>
2015-07-24 15:13:59 +1200

    cmake tweaks

--- CompileOptions.cmake
+++ CompileOptions.cmake
@@ -1,26 +1,35 @@
 
-# don't dereference quoted variables in if() args
-cmake_policy(SET CMP0054 NEW)
+# require new behaviour of:
+# CMP0054 (don't dereference quoted variables in if() args)
+cmake_minimum_required(VERSION 3.1)
 
 # Set GECOM default compile options (warnings etc.)
 function(gecom_add_default_compile_options)
 	if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 		# Full normal warnings, multithreaded build
 		add_compile_options(/W4 /MP)
-		# Ignore C4800: forcing X to bool (performance warning)
+		# Disable C4800: forcing X to bool (performance warning)
 		add_compile_options(/wd4800)
 	elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-		# Full normal warnings
+		# C++14, full normal warnings
 		add_compile_options(-std=c++1y -Wall -Wextra -pedantic)
+		# Threading support
+		add_compile_options(-pthread)
 		# Promote missing return to error
 		add_compile_options(-Werror=return-type)
 		# enable coloured output if gcc >= 4.9
+		# TODO remove this, use the GCC_COLORS env instead?
 		execute_process(COMMAND ${CMAKE_CXX_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION)
 		if (GCC_VERSION VERSION_GREATER 4.9 OR GCC_VERSION VERSION_EQUAL 4.9)
 			add_compile_options(-fdiagnostics-color)
 		endif()
 	elseif("${CMAKE_CXX_COMPILER_ID}" MATCHES "^(Apple)?Clang$")
-		add_compile_options(-std=c++1y -pthread -Wall -Wextra -Werror=return-type -pedantic)
+		# C++14, full normal warnings
+		add_compile_options(-std=c++1y -Wall -Wextra -pedantic)
+		# Threading support
+		add_compile_options(-pthread)
+		# Promote missing return to error
+		add_compile_options(-Werror=return-type)
 	endif()
 endfunction()
 
@@ -39,3 +48,17 @@ function(gecom_target_gcc_compile_options)
 		target_compile_options(${ARGV})
 	endif()
 endfunction()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
