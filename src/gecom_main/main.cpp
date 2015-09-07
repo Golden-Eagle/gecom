f2ba5dce50bf8e0b8a588cd6bc68b01df22ff84d
Ben Allen <benos.9k@gmail.com>
2015-09-08 00:26:56 +1200

    clean up main

--- src/gecom_main/main.cpp
+++ src/gecom_main/main.cpp
@@ -17,69 +17,19 @@ using namespace std::literals;
 int main() {
 	section_guard sec("main");
 
-	// need to reference something from terminal.cpp to initialize redirection
+	// this seems to encourage terminal to init before async
 	cout << terminal::reset << std::endl;
+
+	Log::info().verbosity(0) << "Starting...";
 
 	assertMainThread();
 
-	Log::info().verbosity(4) << "shouldn't see this on console";
-
-	// test
-	auto fut = async::invoke(1s, [](int a, int b) {
-		Log::info() << "ASYNC! : " << (a + b);
-		return a + b;
-	}, 1, 2);
-
-	Log::info() << "FUTURE! : " << fut.get();
-
-	std::atomic<int> c { 0 };
-
-	for (int i = 0; i < 1000; i++) {
-		async::invokeMain(1s, [&] {
-			c++;
-		});
-	}
 	
-	async::execute(1ms);
-
-	Log::info() << "dsghsfh " << c;
-
 	gecom::Window *win = createWindow().title("Hello World").size(640, 480).contextVersion(4, 1).visible(true);
-
-	Log::info("TEST") << "test...";
 
 	win->makeCurrent();
 
-	{
-		section_guard sec("foo");
-		Log::warning() << "this is a warning";
-		{
-			section_guard sec("foo");
-			Log::warning() << "this is still a warning";
-			{
-				section_guard sec("foo");
-				Log::warning() << "this is just a warning";
-				{
-					section_guard sec("foo");
-					Log::warning() << "this used to be a warning, but we can't afford them anymore";
-					{
-						section_guard sec("bar");
-						Log::error() << "this is an error, fix it";
-					}
-				}
-			}
-		}
-	}
-
-	for (int i = 40; i < 60; i++) {
-		section_guard sec("overflow");
-		string s;
-		for (int j = 0; j < i; j++) {
-			s += 'x';
-		}
-		Log::info() << s;
-	}
-
+	
 	while (!win->shouldClose()) {
 		glfwPollEvents();
 		if (win->getKey(GLFW_KEY_SPACE)) {
