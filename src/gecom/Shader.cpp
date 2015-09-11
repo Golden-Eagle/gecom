3548fdf16b1612fc5dd1b4dcf5c2cf360d9daf44
Ben Allen <benos.9k@gmail.com>
2015-09-11 16:24:04 +1200

    strip out old shader manager

---
+++ src/gecom/Shader.cpp
@@ -0,0 +1,23 @@
+
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include <chrono>
+#include <mutex>
+#include <limits>
+
+#include "Util.hpp"
+
+#include "Shader.hpp"
+
+namespace gecom {
+
+
+
+}
